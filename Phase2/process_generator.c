be0e359243cc5822e0c733b2d5a57a84b87da4a7
Moodrammer <mahmou.m.gody@gmail.com>
2021-01-16 16:06:31 +0200

    Add: taking file path as an argument in process generator

--- Phase2/process_generator.c
+++ Phase2/process_generator.c
@@ -21,7 +21,12 @@ int main(int argc, char *argv[])
     */
     FILE *input_file;
     char str[MAXCHAR];
-    char *file_path = "testcases/processes_1.txt";
+    if (argc == 1)
+    {
+        printf("Please provide the file name (path relative to current directory) as an argument !\n");
+        exit(-1);
+    }
+    char *file_path = argv[1];
 
     int number_of_processes = 0, memory_size = 10;
     processes_info = (struct processInfo *)malloc(sizeof(struct processInfo) * memory_size);
